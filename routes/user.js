b8cca3f29919bd01254942a58401a80c99430809
monukanyal <monu_kanyal@esferasoft.com>
2018-01-15 16:00:43 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const passport = require('passport');
+const flash = require('connect-flash');
 const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 const router = express.Router();
 
 /* GET user profile. */
 router.get('/', ensureLoggedIn, function(req, res, next) {
 
-	res.send('user data'+req.user);
-  // res.render('user', {
-  //   user: req.user
-  // });
+	 res.render('dashboard'	,{message:req.flash('success'),email:req.session.passport.user[0]._id});
+	
 });
+
 
 module.exports = router;
